4e96371a2068dc7eff93ce6ad67746c28650bb2e
troywickliffe <108986639+troywickliffe@users.noreply.github.com>
2022-10-31 18:01:00 -0400

    Add function for creating todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import {v4 as uuidv4} from "uuid"
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
     <>
-      <TodoList />
-      <input type="text" />
-      <button>Add Task</button>
+      <TodoList todos={todos} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Task</button>
       <button>Clear Task</button>
       <div>0 left to do</div>
     </>
