6983a3c3200b611f30c53c365298b1638a5b06a8
troywickliffe <108986639+troywickliffe@users.noreply.github.com>
2022-10-31 19:01:24 -0400

    Update checked prop to defaultChecked

--- src/Todo.js
+++ src/Todo.js
@@ -4,7 +4,7 @@ export default function Todo({ todo }) {
   return (
     <div>
         <label>
-            <input type="checkbox" checked={todo.complete} />
+            <input type="checkbox" defaultChecked={todo.complete} />
             {todo.name}
         </label>
     </div>
